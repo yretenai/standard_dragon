1781ee0b6195288213f8bf6e8ed52239e522dd02
Naomi <yretenai@gmail.com>
2020-08-17 08:28:47 +0100

    [wembnk] parse bkhd

--- WemSoundbank.hpp
+++ WemSoundbank.hpp
@@ -23,29 +23,37 @@ namespace dragon {
             uint32_t length = 0;
         } DataIndexEntry;
 #pragma pack(pop)
+        const uint32_t BKHD_FOURCC = MAKEFOURCC('B', 'K', 'H', 'D');
         const uint32_t DIDX_FOURCC = MAKEFOURCC('D', 'I', 'D', 'X');
         const uint32_t DATA_FOURCC = MAKEFOURCC('D', 'A', 'T', 'A');
         WemSoundbank(dragon::Array<char> buffer) {
-            BaseStream = std::make_shared<dragon::Array<char>>(buffer.data(), buffer.byte_size(), nullptr);
+            base_stream = std::make_shared<dragon::Array<char>>(buffer.data(), buffer.byte_size(), nullptr);
             uintptr_t ptr = 0;
-            while (ptr < BaseStream->size()) {
-                BnkChunkHeader header = BaseStream->lpcast<BnkChunkHeader>(&ptr);
-                Chunks[header.fourcc] = std::make_shared<dragon::Array<char>>(BaseStream, ptr, header.size);
+            while (ptr < base_stream->size()) {
+                BnkChunkHeader header = base_stream->lpcast<BnkChunkHeader>(&ptr);
+                chunks[header.fourcc] = std::make_shared<dragon::Array<char>>(base_stream, ptr, header.size);
             }
-            if (Chunks.find(DIDX_FOURCC) != Chunks.end() && Chunks.find(DATA_FOURCC) != Chunks.end()) {
-                std::shared_ptr<dragon::Array<char>> didx_stream = Chunks[DIDX_FOURCC];
-                std::shared_ptr<dragon::Array<char>> data_stream = Chunks[DATA_FOURCC];
+            if (chunks.find(BKHD_FOURCC) != chunks.end()) {
+                std::shared_ptr<dragon::Array<char>> bkhd_stream = chunks[BKHD_FOURCC];
+                version = bkhd_stream->cast<uint32_t>(0);
+                id = bkhd_stream->cast<uint32_t>(4);
+            }
+            if (chunks.find(DIDX_FOURCC) != chunks.end() && chunks.find(DATA_FOURCC) != chunks.end()) {
+                std::shared_ptr<dragon::Array<char>> didx_stream = chunks[DIDX_FOURCC];
+                std::shared_ptr<dragon::Array<char>> data_stream = chunks[DATA_FOURCC];
                 dragon::Array<DataIndexEntry> didx = didx_stream->cast<DataIndexEntry>(0, didx_stream->size() / sizeof(DataIndexEntry));
                 for (DataIndexEntry entry : didx) {
-                    Streams[entry.stream_id] = std::make_shared<dragon::Array<char>>(data_stream, entry.offset, entry.length);
+                    streams[entry.stream_id] = std::make_shared<dragon::Array<char>>(data_stream, entry.offset, entry.length);
                 }
             }
         }
 
         ~WemSoundbank() = default;
-        std::shared_ptr<dragon::Array<char>> BaseStream;
-        std::map<uint32_t, std::shared_ptr<dragon::Array<char>>> Chunks;
-        std::map<uint32_t, std::shared_ptr<dragon::Array<char>>> Streams;
+        std::shared_ptr<dragon::Array<char>> base_stream;
+        std::map<uint32_t, std::shared_ptr<dragon::Array<char>>> chunks;
+        std::map<uint32_t, std::shared_ptr<dragon::Array<char>>> streams;
+        uint32_t version;
+        uint32_t id;
     };
 } // namespace dragon
 
