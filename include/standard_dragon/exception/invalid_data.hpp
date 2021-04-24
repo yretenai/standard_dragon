985bbeb168dfcac79994bcec94b416483579674f
Lilith Naomi Nightingale <yretenai@gmail.com>
2021-04-24 19:15:20 +0100

    [exception] std::string constructors

--- include/standard_dragon/exception/invalid_data.hpp
+++ include/standard_dragon/exception/invalid_data.hpp
@@ -6,13 +6,15 @@
 
 #include <exception>
 #include <string>
+#include <utility>
 
 namespace dragon::exception {
     class invalid_data : public std::exception {
       public:
         std::string reason_message;
         explicit invalid_data(const char* message = "invalid or unexpected data") { reason_message = message; }
+        explicit invalid_data(std::string message) { reason_message = std::move(message); }
 
-        const char* what() const noexcept override { return reason_message.c_str(); }
+        [[nodiscard]] const char* what() const noexcept override { return reason_message.c_str(); }
     };
 } // namespace dragon::exception
--- include/standard_dragon/exception/not_implemented.hpp
+++ include/standard_dragon/exception/not_implemented.hpp
@@ -6,12 +6,14 @@
 
 #include <exception>
 #include <string>
+#include <utility>
 
 namespace dragon::exception {
     class not_implemented : public std::exception {
       public:
         std::string reason_message;
         explicit not_implemented(const char* message = "not implemented") { reason_message = message; }
+        explicit not_implemented(std::string message) { reason_message = std::move(message); }
 
         const char* what() const noexcept override { return reason_message.c_str(); }
     };
