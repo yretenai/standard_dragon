2a32f33b2f4ad64098230d23322c9d6152e1c3f1
Lilith Naomi Nightingale <yretenai@gmail.com>
2021-06-13 13:42:32 +0100

    [fnv] fix issue when header is included in multiple projects

--- include/standard_dragon/hash/fnv1a.hpp
+++ include/standard_dragon/hash/fnv1a.hpp
@@ -8,12 +8,12 @@
 #include <cstdint>
 
 namespace dragon::hash {
-    static const uint32_t FNV_PRIME_32 = 0x01000193;
-    static const uint32_t FNV_BASIS_32 = 0x811C9DC5;
-    static const uint64_t FNV_PRIME_64 = 0x00000100000001B3;
-    static const uint64_t FNV_BASIS_64 = 0xCBF29CE484222325;
+    constexpr uint32_t FNV_PRIME_32 = 0x01000193;
+    constexpr uint32_t FNV_BASIS_32 = 0x811C9DC5;
+    constexpr uint64_t FNV_PRIME_64 = 0x00000100000001B3;
+    constexpr uint64_t FNV_BASIS_64 = 0xCBF29CE484222325;
 
-    uint32_t fnv1a32(const uint8_t *buf, size_t size, uint32_t bits = 32, uint32_t basis = FNV_BASIS_32, uint32_t prime = FNV_PRIME_32) {
+    inline uint32_t fnv1a32(const uint8_t *buf, size_t size, uint32_t bits = 32, uint32_t basis = FNV_BASIS_32, uint32_t prime = FNV_PRIME_32) {
         for (size_t i = 0; i < size; ++i) {
             basis ^= buf[i];
             basis *= prime;
@@ -24,7 +24,7 @@ namespace dragon::hash {
         return basis & ((1 << bits) - 1);
     }
 
-    uint64_t fnv1a64(const uint8_t *buf, size_t size, uint64_t bits = 64, uint64_t basis = FNV_BASIS_64, uint64_t prime = FNV_PRIME_64) {
+    inline uint64_t fnv1a64(const uint8_t *buf, size_t size, uint64_t bits = 64, uint64_t basis = FNV_BASIS_64, uint64_t prime = FNV_PRIME_64) {
         for (size_t i = 0; i < size; ++i) {
             basis ^= buf[i];
             basis *= prime;
@@ -34,7 +34,7 @@ namespace dragon::hash {
         return basis & ((1 << bits) - 1);
     }
 
-    uint32_t fnv1_32(const uint8_t *buf, size_t size, uint32_t bits = 32, uint32_t basis = FNV_BASIS_32, uint32_t prime = FNV_PRIME_32) {
+    inline uint32_t fnv1_32(const uint8_t *buf, size_t size, uint32_t bits = 32, uint32_t basis = FNV_BASIS_32, uint32_t prime = FNV_PRIME_32) {
         for (size_t i = 0; i < size; ++i) {
             basis *= prime;
             basis ^= buf[i];
@@ -44,7 +44,7 @@ namespace dragon::hash {
         return basis & ((1 << bits) - 1);
     }
 
-    uint64_t fnv1_64(const uint8_t *buf, size_t size, uint32_t bits = 64, uint64_t basis = FNV_BASIS_64, uint64_t prime = FNV_PRIME_64) {
+    inline uint64_t fnv1_64(const uint8_t *buf, size_t size, uint32_t bits = 64, uint64_t basis = FNV_BASIS_64, uint64_t prime = FNV_PRIME_64) {
         for (size_t i = 0; i < size; ++i) {
             basis *= prime;
             basis ^= buf[i];
